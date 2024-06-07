ead96eafa8adca6cb43a89a8f432add57f1aafc9
FrancescoCesari97 <freedus97@gmail.com>
2024-06-07 12:14:51 +0200

    Variables and type of data

--- Main.java
+++ Main.java
@@ -2,6 +2,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("x");
+        // * VARIABLES */
+        // int x = 45;
+        // String y = "Hello";
+        // double w = 2.34;
+
+        // x = 25;
+
+        String name;
+        int age;
+
+        name = "freedus";
+        age = 26;
+
+        System.out.println(name);
+        System.out.println(age);
+
     }
 }
---
+++ dataType.java
@@ -0,0 +1,15 @@
+public class dataType {
+
+    // bolean | true : false | 1 bit
+    // byte | -128 : 127 | 1 byte
+    // short | -32768 : 32767 | 2 bytes
+    // int | -2 billion : 2 billion | 4 bytes
+    // long | -9 quintillion : 9 quintillion | 8 bytes
+
+    // float | 6-7 decimal number | 4 bytes
+    // double | 15 decimal number | 8 bytes
+
+    // char | single character | 2 bytes
+    // String | sequence of character | variable
+
+}
