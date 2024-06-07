7576ac2185e28cd81b14435d58285c675a8cd166
FrancescoCesari97 <freedus97@gmail.com>
2024-06-07 12:27:34 +0200

    print test data type

--- dataType.java
+++ dataType.java
@@ -11,5 +11,22 @@ public class dataType {
 
     // char | single character | 2 bytes
     // String | sequence of character | variable
+    public static void main(String[] args) {
+
+        boolean theBoolean = true;
+        byte theByte = 127;
+        short theShort = 32767;
+        int theInt = 2_000_000_000;
+        long theLong = 9_000_000_000_000_000_000L;
+
+        float theFloat = 2.2549225f;
+        double theDouble = 3.236985147826594;
+
+        char theChar = 'c'; // con il singolo apice indica il char
+        String theString = "freedus"; // con il doppio apice per le stringe
+
+        System.out.println(theLong);
+
+    }
 
 }
