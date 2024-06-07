dd25ffd256851152ef898aa8f1554a0e9c9d0cfa
FrancescoCesari97 <freedus97@gmail.com>
2024-06-07 16:20:14 +0200

    print test arithmetic operations and class Math

---
+++ arithmeticOperations.java
@@ -0,0 +1,23 @@
+public class arithmeticOperations {
+
+    public static void main(String[] args) {
+
+        // int x = 10;
+        // int y = 2;
+        // int z = x % y;
+        // x += 3; // x = x + 3;
+
+        // System.out.println(Math.abs(-55));
+
+        // System.out.println(Math.min(81, 41));
+
+        // System.out.println(Math.max(81, 41));
+
+        // System.out.println(Math.pow(7, 3));
+
+        // System.out.println(Math.sqrt(30));
+
+        System.out.println(Math.PI);
+    }
+
+}
