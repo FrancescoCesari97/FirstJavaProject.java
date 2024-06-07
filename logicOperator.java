263523d30c74b5d242d1b211597b0824ae6dd123
FrancescoCesari97 <freedus97@gmail.com>
2024-06-07 17:44:39 +0200

    added GUI graphical user interface and logic operators

---
+++ logicOperator.java
@@ -0,0 +1,23 @@
+public class logicOperator {
+
+    public static void main(String[] args) {
+
+        // int x = 10;
+        // int y = 12;
+        // boolean result = 3 > 10;
+
+        // boolean x = 3 < 10 && 3 % 2 == 0;
+        // boolean x = 3 < 10 || 3 % 2 == 0;
+
+        // !/ if statement
+
+        boolean isOnline = true;
+
+        if (isOnline) {
+            System.out.println("is Online");
+        }
+
+        // System.out.println(!(3 > 10));
+    }
+
+}
