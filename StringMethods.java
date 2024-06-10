cf19f57ce4a2f5799743987c99b224170070b621
FrancescoCesari97 <freedus97@gmail.com>
2024-06-10 19:20:33 +0200

    print test String method

--- StringMethods.java
+++ StringMethods.java
@@ -1,3 +1,22 @@
 public class StringMethods {
 
+    public static void main(String[] args) {
+
+        // ! equals method
+
+        String name = "  freedus   ";
+
+        // boolean result = name.equalsIgnoreCase("Freedus");
+        // int result = name.length();
+        // char result = name.charAt(0);
+        // int result = name.indexOf("e");
+        // boolean result = name.isEmpty();
+        // String result = name.toUpperCase();
+        // String result = name.toLowerCase();
+        // String result = name.trim();
+        String result = name.replace('e', 'i');
+
+        System.out.println(result);
+    }
+
 }
