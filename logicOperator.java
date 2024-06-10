294185b06bd85a830b65c5e1dc82e6947d79eb4b
FrancescoCesari97 <freedus97@gmail.com>
2024-06-10 12:04:56 +0200

    print test Switch

--- logicOperator.java
+++ logicOperator.java
@@ -11,13 +11,60 @@ public class logicOperator {
 
         // !/ if statement
 
-        boolean isOnline = true;
+        // boolean isOnline = true;
 
-        if (isOnline) {
-            System.out.println("is Online");
-        }
+        // if (isOnline) {
+        // System.out.println("is Online");
+        // }
+
+        // int age = 17;
+
+        // if (age >= 18) {
+        // System.out.println("you can drink alchol");
+        // } else {
+        // System.out.println("you can't drink alchol");
+        // }
 
         // System.out.println(!(3 > 10));
+
+        // !/ switch statement allows a variable to be tested for equality against a
+        // list of values
+
+        String day = "prova";
+
+        switch (day) {
+
+            case "Monday":
+                System.out.println("it's Monday");
+                break;
+            case "Tuesday":
+                System.out.println("it's Tuesday");
+                break;
+            case "Wednesady":
+                System.out.println("it's Wednesady");
+                break;
+            case "Thursday":
+                System.out.println("it's Thursday");
+                break;
+
+            case "Friday":
+                System.out.println("it's Friday");
+                break;
+
+            case "Saturday":
+                System.out.println("it's Saturday");
+                break;
+
+            case "Sunday":
+                System.out.println("it's Sunday");
+                break;
+
+            default:
+                System.out.println("it's not a day");
+                break;
+        }
+
+        // !/
     }
 
 }
