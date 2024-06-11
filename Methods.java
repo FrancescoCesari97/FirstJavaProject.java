87302d62e6dd3a9a5e0d3e0834bb33099d511efc
FrancescoCesari97 <freedus97@gmail.com>
2024-06-11 12:48:00 +0200

    print test methods

---
+++ Methods.java
@@ -0,0 +1,38 @@
+public class Methods {
+
+    public static void main(String[] args) {
+        // method = a block of code that is executed whenever it is called upon
+
+        // String name = "freedus";
+
+        // int age = 26;
+
+        // hello(name, age);
+
+        // ! return type
+
+        int x = 3;
+        int y = 4;
+
+        // int z = add(x, y);
+        // System.out.println(z);
+
+        System.out.println(add(x, y));
+
+    }
+
+    // static void hello(String name, int age) {
+    // System.out.println("Hello " + name + " you're " + age);
+    // }
+
+    // ! return type
+
+    static int add(int x, int y) {
+
+        int z = x + y;
+
+        return z;
+
+    }
+
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+
+    static void hello(String name) {
